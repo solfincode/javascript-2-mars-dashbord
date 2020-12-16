1b6b98d1adb6a787fad35dc6553ae24b7f46a867
Q <coderinch@gmail.com>
2020-12-16 19:52:56 +0900

    state on immutable js not working

--- src/server/app.js
+++ src/server/app.js
@@ -1,18 +1,21 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const fetch = require("node-fetch");
 const path = require("path");
+const cors = require("cors");
 
 const PORT = process.env.PORT;
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use("/", express.static(path.join(__dirname, "../frontend")));
 
 //API CALL
-app.get("/", async (req, res) => {
+app.get("/apod", async (req, res) => {
   try {
     let image = await fetch(
       `https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`
